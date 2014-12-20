c338f678568cdb4f1d2cc56fe4fe00895d489b40
philipjrosen <philipjrosen@gmail.com>
2014-12-19 17:27:45 -0800

    adds player and enemies to svg element

--- watchout.js
+++ watchout.js
@@ -1,2 +1,33 @@
-// start slingin' some d3 here.
+var stage = d3.select('body')
+              .append('svg')
+              .attr('width', 800)
+              .attr('height', 600);
 
+var svgPlayer = stage.append('circle');
+
+var enemies = d3.range(0,50).map(function(enemy){
+  return enemy;
+});
+
+svgPlayer.attr({'class':'player', 'cx': 400, 'cy':300, 'r':10});
+
+svgPlayer.on('click', function(e){
+  console.log('clicked');
+});
+
+var update = function(data){
+
+  stage.selectAll('.enemy')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr({'class':'enemy', 'r':10})
+    .attr('cy', function(d, i){
+      return (Math.random() * 600);
+    })
+    .attr('cx', function(d, i){
+      return (Math.random() * 800);
+    });
+}
+
+update(enemies);
