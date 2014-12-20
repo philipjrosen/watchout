84c4e7925814856a2bd3c15aa42f795cab95d040
philipjrosen <philipjrosen@gmail.com>
2014-12-19 19:18:07 -0800

    enemies transition randomly

--- watchout.js
+++ watchout.js
@@ -15,19 +15,32 @@ svgPlayer.on('click', function(e){
   console.log('clicked');
 });
 
-var update = function(data){
+stage.selectAll('.enemy')
+  .data(enemies)
+  .enter()
+  .append('circle')
+  .attr({'class':'enemy', 'r':10})
+  .attr('cy', function(d, i){
+    return (Math.random() * 600);
+  })
+  .attr('cx', function(d, i){
+    return (Math.random() * 800);
+  });
 
+var update = function(){
   stage.selectAll('.enemy')
-    .data(data)
-    .enter()
-    .append('circle')
-    .attr({'class':'enemy', 'r':10})
+    .transition()
+    .duration(1000)
     .attr('cy', function(d, i){
       return (Math.random() * 600);
     })
     .attr('cx', function(d, i){
       return (Math.random() * 800);
     });
-}
+};
 
 update(enemies);
+
+setInterval(function(){
+  update(enemies);
+},1000);
